cc473f44f030bd2842e90d365d1154f76220a3f8
Norbert Vígh <norbertusvigh@gmail.com>
2023-11-11 00:14:57 +0100

    tommit

--- Assets/Scripts/Paper.cs
+++ Assets/Scripts/Paper.cs
@@ -5,26 +5,12 @@ using UnityEngine;
 
 public class Paper : MonoBehaviour
 {
+    public GameObject[] words;
     
-    public GameObject textLinePrefab;
-    
-    public int numLines = 10;
-    
-    private int maxNumLines = 19;
-
     // Start is called before the first frame update
     void Start()
     {
-        if (numLines > maxNumLines)
-        {
-            numLines = maxNumLines;
-        }
         
-        for (int i = 0; i < numLines; i++)
-        {
-            GameObject textLine = Instantiate(textLinePrefab, transform);
-            textLine.transform.localPosition = new Vector3(0, 0.001f, 4.45f - 0.5f * i);
-        }
     }
 
     // Update is called once per frame
